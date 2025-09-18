ccc6b58aa38ab0688bf02b15b0cea3d71a7f5d7b
Nishadpachghare <pachgharenishad05@gmail.com>
2025-09-18 13:29:08 +0530

    fix some errors2

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero';
 import Platform from './components/Platform';
 import Work from './components/Work';
 import Service from './components/Service2';
-import Key from './components/key';
+import Key from './components/Key';
 import Footer from './layout/Footer';
 
 const Home = () => {
